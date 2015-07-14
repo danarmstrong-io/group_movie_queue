afd3704d797f9704d9b0b921eac55141d9dc8f5c
Daniel Armstrong <wtfdaboo@gmail.com>
2015-07-14 15:03:11 -0700

    finished start modal and lists modal

--- app/assets/javascripts/angular/controllers/lists-modal-controller.js
+++ app/assets/javascripts/angular/controllers/lists-modal-controller.js
@@ -1,10 +1,12 @@
-app.controller('ListsModalController', function ($scope, $modalInstance, QueuedLists, UserFactory, ReadyList, $state, $stateParams) {
+app.controller('ListsModalController', function ($scope, $modalInstance, QueuedLists, UserFactory, ReadyList, $state, $stateParams, $http) {
 
 	$scope.init = function() {
     $scope.queuedLists = QueuedLists.listsData;
 		$scope.modalInstance = $modalInstance;
    	$scope.userData = UserFactory.userData;
    	$scope.modalInstance = $modalInstance;
+		$scope.createListData = {invitees: [], title: ""};
+		$scope.createList = false;
 	};
 
 	$scope.selectList = function(listId) {
@@ -19,6 +21,86 @@ app.controller('ListsModalController', function ($scope, $modalInstance, QueuedL
 	$scope.switchToLists = function() {
 		$scope.createList = false;
 	};
+
+	$scope.addInvitee = function() {
+		$scope.createListData.invitees.push({email: ""});
+	};
+
+	$scope.removeInvitee = function(index) {
+		$scope.createListData.invitees.splice(index, 1);
+	};
+
+	$scope.checkIfActiveUser = function(user) {
+		user.checked = true;
+		$http({ url: '/api/v1/show_by_email/', method: "GET", params: {email: user.email} }).
+	    success(function(data) {
+	    	user.exists = JSON.parse(data);
+	    	setUserTooltipInfo(user);
+	    }).
+      error(function(data) {
+      	console.log("error")
+      });
+	};
+
+	$scope.postList = function() {
+		$http.post('/api/v1/queued_lists', $scope.createListData).
+			success(function(data) {
+	    	ReadyList.changeListId(data.id)
+				QueuedLists.updateLists();
+	    	$state.go('dashboard.readyList');
+	    	if ($scope.modalInstance) {
+	    		$scope.modalInstance.dismiss('cancel');
+	    	}
+
+	    }).
+      error(function(data) {
+      	console.log("error")
+      });
+	}
+
+	$scope.inviteeExistsAndChecked = function(invitee) {
+		return (invitee.exists && invitee.checked);
+	}
+
+	$scope.inviteeDoesntExistAndChecked = function(invitee) {
+		return (!invitee.exists && invitee.checked);
+	}
+
+	$scope.invalidEmail = function(invitee) {
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return (!re.test(invitee.email) && invitee.checked);
+	}
+
+	$scope.invalidTitleOrInvalidEmails = function() {
+		if ($scope.createListData.title == "") {
+			return true;
+		}
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		for(var index = 0; index < $scope.createListData.invitees.length; index++) {
+			if ($scope.createListData.invitees[index].email != "") {
+		  	if (!re.test($scope.createListData.invitees[index].email)) {
+		  		return true;
+		  	}
+		  }
+		}
+		return false;
+	}
+
+	$scope.hasInvitees = function() {
+		return ($scope.createListData.invitees.length > 0)
+	}
+
+	function setUserTooltipInfo(user) {
+		if (user.exists) {
+  		user.tooltipInfo = "User will be invited to join this list.";
+  	}
+  	else {
+  		user.tooltipInfo = "User does not exist and will be invited to Oogway's list by email.";
+  	}
+  	if ($scope.invalidEmail(user)) {
+  		user.tooltipInfo = "Invalid Email";
+  	}
+	}
 
 	$scope.init();
 	
--- app/assets/javascripts/angular/controllers/start-modal-controller.js
+++ app/assets/javascripts/angular/controllers/start-modal-controller.js
@@ -4,15 +4,101 @@ app.controller('StartModalController', function ($scope, $modalInstance, QueuedL
     $scope.queuedLists = QueuedLists.listsData;
 		$scope.modalInstance = $modalInstance;
    	$scope.userData = UserFactory.userData;
+		$scope.createListData = {invitees: [], title: ""};
+	};
+
+	$scope.selectList = function(listId) {
+		ReadyList.changeListId(listId);
+    $modalInstance.dismiss('cancel');
 	};
 
 	$scope.switchToCreateList = function() {
 		$scope.createList = true;
 	};
 
-	$scope.switchToLists = function() {
+	$scope.switchToInvites = function() {
 		$scope.createList = false;
 	};
+
+	$scope.addInvitee = function() {
+		$scope.createListData.invitees.push({email: ""});
+	};
+
+	$scope.removeInvitee = function(index) {
+		$scope.createListData.invitees.splice(index, 1);
+	};
+
+	$scope.checkIfActiveUser = function(user) {
+		user.checked = true;
+		$http({ url: '/api/v1/show_by_email/', method: "GET", params: {email: user.email} }).
+	    success(function(data) {
+	    	user.exists = JSON.parse(data);
+	    	setUserTooltipInfo(user);
+	    }).
+      error(function(data) {
+      	console.log("error")
+      });
+	};
+
+	$scope.postList = function() {
+		$http.post('/api/v1/queued_lists', $scope.createListData).
+			success(function(data) {
+	    	ReadyList.changeListId(data.id)
+				QueuedLists.updateLists();
+	    	$state.go('dashboard.readyList');
+	    	if ($scope.modalInstance) {
+	    		$scope.modalInstance.dismiss('cancel');
+	    	}
+
+	    }).
+      error(function(data) {
+      	console.log("error")
+      });
+	}
+
+	$scope.inviteeExistsAndChecked = function(invitee) {
+		return (invitee.exists && invitee.checked);
+	}
+
+	$scope.inviteeDoesntExistAndChecked = function(invitee) {
+		return (!invitee.exists && invitee.checked);
+	}
+
+	$scope.invalidEmail = function(invitee) {
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return (!re.test(invitee.email) && invitee.checked);
+	}
+
+	$scope.invalidTitleOrInvalidEmails = function() {
+		if ($scope.createListData.title == "") {
+			return true;
+		}
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		for(var index = 0; index < $scope.createListData.invitees.length; index++) {
+			if ($scope.createListData.invitees[index].email != "") {
+		  	if (!re.test($scope.createListData.invitees[index].email)) {
+		  		return true;
+		  	}
+		  }
+		}
+		return false;
+	}
+
+	$scope.hasInvitees = function() {
+		return ($scope.createListData.invitees.length > 0)
+	}
+
+	function setUserTooltipInfo(user) {
+		if (user.exists) {
+  		user.tooltipInfo = "User will be invited to join this list.";
+  	}
+  	else {
+  		user.tooltipInfo = "User does not exist and will be invited to Oogway's list by email.";
+  	}
+  	if ($scope.invalidEmail(user)) {
+  		user.tooltipInfo = "Invalid Email";
+  	}
+	}
 
 	$scope.membersString = function(invite) {
 		var string = "";
